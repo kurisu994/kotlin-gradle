8ef098681b7d9ce8affbe2b0d516cbbedfc06d84
Kurisu <lucifer994@gmail.com>
2024-01-30 00:02:59 +0800

    :fire:

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,12 +3,13 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.2.0"
     id("io.spring.dependency-management") version "1.1.4"
-    kotlin("jvm") version "1.9.21"
-    kotlin("plugin.spring") version "1.9.21"
+    kotlin("jvm") version "1.9.22"
+    kotlin("plugin.spring") version "1.9.22"
 }
 
 group = "cn.hutao.buer"
 version = "0.0.1"
+
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
@@ -29,9 +30,9 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter")
     implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.21")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.22")
     implementation("com.baomidou:mybatis-plus:3.5.4.1")
-    implementation("cn.hutool:hutool-all:5.8.23")
+    implementation("cn.hutool:hutool-all:5.8.25")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
