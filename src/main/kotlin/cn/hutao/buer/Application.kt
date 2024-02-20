c73f6878f669aa0893190d2d52b0f8a70ac2c735
Kurisu <makise_kurisuu@outlook.jp>
2024-02-20 18:06:11 +0800

    :rocket: update

--- src/main/kotlin/cn/hutao/buer/Application.kt
+++ src/main/kotlin/cn/hutao/buer/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class KotlinGradleApplication
+class Application
 
 fun main(args: Array<String>) {
-    runApplication<KotlinGradleApplication>(*args)
+    runApplication<Application>(*args)
 }
